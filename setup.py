bbe3efa1c8aca832919b45785f5d7a2456edcf59
Juan Lara <julara@unal.edu.co>
2021-01-24 18:22:53 -0500

    automatic requirements from setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt", "r") as f:
+    requirements = f.readlines()
+
 setup(
         name="dmae",
         version="1.1",
         author="Juan S. Lara",
         author_email="julara@unal.edu.co",
         packages=find_packages(),
+        install_requires=requirements,
         description="TensorFlow implementation of the dissimilarity mixture autoencoder (DMAE)",
         license="MIT"
 )
